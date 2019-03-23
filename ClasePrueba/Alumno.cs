1289bbf9e7b79225e1b3f23c8e064545c895f95a
Rodrigoono1 <rodrigo.javier.ono@gmail.com>
2019-03-22 21:40:37 -0300

    primer programa C#

---
+++ ClasePrueba/Alumno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasePrueba
+{
+    public class Alumno
+    {
+        public String Nombre { get; set;}
+        public String Apellido { get; set; }
+        public int MyProperty { get; set; }
+        public Alumno(){
+
+        }
+        public Alumno(String nombre, String apellido) {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+        }
+    }   
+}
---
+++ Clases/Program.cs
@@ -0,0 +1,26 @@
+﻿using ClasePrueba;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno personaje = new Alumno();
+            personaje.Nombre = "Martin";
+            personaje.Apellido = "Guasd";
+
+            Alumno ono = new Alumno { Nombre = "Ono", Apellido = "Rodrigo" };
+
+            Alumno prueba = new Alumno("Jose", "Yegros");
+
+            Console.WriteLine("Fin del programa, Presione cualquier tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
